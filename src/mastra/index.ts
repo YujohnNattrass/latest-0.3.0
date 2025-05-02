0f565f2de81072cc34848a99c9ba978fbcec917e
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-05-01 18:10:43 -0700

    again!

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -3,10 +3,12 @@ import { Mastra } from '@mastra/core/mastra';
 import { LibSQLStore } from '@mastra/libsql';
 import { weatherWorkflow } from './workflows';
 import { weatherAgent } from './agents';
-
+import { PineconeVector } from '@mastra/pinecone';
+const pinecone = new PineconeVector(process.env.PINECONE_API_KEY!);
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
+  vectors: {pinecone: pinecone},
   storage: new LibSQLStore({
     url: 'file:../mastra.db',
    }),
