193c6dead929ac1d8fbe7b70c28246d564e6ecd7
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-05-22 00:20:52 -0700

    push

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -3,7 +3,6 @@ import { Mastra } from '@mastra/core/mastra';
 import { LibSQLStore } from '@mastra/libsql';
 import { weatherWorkflow } from './workflows';
 import { weatherAgent } from './agents';
-import { PineconeVector } from '@mastra/pinecone';
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
